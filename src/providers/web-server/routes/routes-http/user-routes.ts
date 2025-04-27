037f6544fe7ad3f3a7ab280390dcb9588b0a9459
mathrocks19 <adsmatheusviana2023@gmail.com>
2025-04-27 12:31:44 -0300

    feat: adicionando rotas de usuário no servidor

---
+++ src/providers/web-server/routes/routes-http/user-routes.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import { sendCreateUserController } from "../../../../app/send-create-user";
+import { IRouterHttp } from "../routes";
+
+export class UserRouter implements IRouterHttp {
+    /**
+     * Handler
+     * @param router
+     */
+    handle(router: express.Router): void {
+        router.post('/user', async (req: Request, resp: Response) => {
+            return sendCreateUserController.handle(req, resp);
+        });
+    }
+}
---
+++ src/providers/web-server/routes/routes.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { UserRouter } from "./routes-http/user-routes"
+
+export interface IRouterHttp {
+    handle(router: express.Router): void
+}
+
+const router = express.Router();
+
+const listRoutes: Array<IRouterHttp> = [
+    new UserRouter()
+];
+
+listRoutes.map((routerCustom) => {
+    routerCustom.handle(router);
+});
+
+export { router };
